f5b13ce2784338331b15e78a9b418368799c7f33
Fabian Schuetz <fscz@me.com>
2023-07-07 10:02:55 +0200

    switch to fastapi

--- hostconf/app/hostconf.py
+++ hostconf/app/hostconf.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from fastapi import FastAPI, HTTPException, Response
 from schema import Schema, Optional, SchemaError, Regex, Use
 import yaml
 import os
@@ -68,73 +68,73 @@ def get_host_config(client):
         for ifname, interface in host.get('interfaces').items():
             if interface.get('mac') == client:
                 return hostname, host
-    return None, None
+    raise HTTPException(status_code=404, detail="Host not found: %s" % client)
 
-def return_if_found(host, fn):
-    if host is not None:
-        return fn(host), 200
-    else:
-        return "Not found", 404
 
-app = Flask(__name__)
+app = FastAPI(title=__name__)
 env = Environment(
     loader=PackageLoader(__name__),
     autoescape=select_autoescape()
 )
 
-@app.route("/osconfig/<client>")
-def install(client):
+
+@app.get("/osconfig/{client}")
+async def osconfig(client):
     hostname, host = get_host_config(client)
     template = env.get_template("osconfig.j2")
-    return return_if_found(host,
-                           lambda host: template.render({
-                               'install': host.get('install'),
-                               'install_to': host.get('install_to'),
-                               'config': host.get('config')
-                           }))
 
-@app.route("/network-config/<client>")
-def network_config(client):
+    return Response(content=template.render({
+        'install': host.get('install'),
+        'install_to': host.get('install_to'),
+        'config': host.get('config')
+    }), media_type="text/string")
+
+
+@app.get("/network-config/{client}")
+async def network_config(client):
     hostname, host = get_host_config(client)
     template = env.get_template("network-config.j2")
-    return return_if_found(host,
-                           lambda host: template.render({
-                               'interfaces':  host.get('interfaces', [])
-                           }))
 
-@app.route("/user-data/<client>")
-def user_data(client):
+    return Response(content=template.render({
+        'interfaces':  host.get('interfaces', [])
+    }), media_type="text/yaml")
+
+
+@app.get("/user-data/{client}")
+async def user_data(client):
     hostname, host = get_host_config(client)
     template = env.get_template("user-data.j2")
-    return return_if_found(host,
-                           lambda host: template.render({
-                               'hostname': hostname,
-                               'users': host.get('users', []),
-                               'groups': host.get('groups', []),
-                               'root_pw': host.get('root_pw', None),
-                               'run_cmds': host.get('run_cmds', []),
-                           }))
 
-@app.route("/meta-data/<client>")
-def meta_data(client):
+    return Response(content=template.render({
+        'hostname': hostname,
+        'users': host.get('users', []),
+        'groups': host.get('groups', []),
+        'root_pw': host.get('root_pw', None),
+        'run_cmds': host.get('run_cmds', []),
+    }), media_type="text/yaml")
+
+
+@app.get("/meta-data/{client}")
+async def meta_data(client):
     hostname, host = get_host_config(client)
     template = env.get_template("meta-data.j2")
-    return return_if_found(host,
-                           lambda host: template.render({
-                               'instance_id': uuid.uuid4(),
-                               'hostname': hostname
-                           }))
 
-@app.route("/unattend/<client>")
-def unattend(client):
+    return Response(content=template.render({
+        'instance_id': uuid.uuid4(),
+        'hostname': hostname
+    }), media_type="text/yaml")
+
+
+@app.get("/unattend/{client}")
+async def unattend(client):
     hostname, host = get_host_config(client)
     template = env.get_template("unattend.xml.j2")
-    return return_if_found(host,
-                           lambda host: template.render({
-                               'hostname': hostname,
-                               'users': host.get('users', []),
-                               'groups': host.get('groups', []),
-                               'root_pw': host.get('root_pw', None),
-                               'run_cmds': host.get('run_cmds', []),
-                               'interfaces': host.get('interfaces', [])
-                           }))
+
+    return Response(content=template.render({
+        'hostname': hostname,
+        'users': host.get('users', []),
+        'groups': host.get('groups', []),
+        'root_pw': host.get('root_pw', None),
+        'run_cmds': host.get('run_cmds', []),
+        'interfaces': host.get('interfaces', [])
+    }), media_type="application/xml")
