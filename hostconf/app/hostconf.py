fccdca68611bed447efbdc8f3611d0e40c45f16a
Fabian Schuetz <fscz@me.com>
2023-07-08 17:04:28 +0200

    add log endpoint

--- hostconf/app/hostconf.py
+++ hostconf/app/hostconf.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Request, Response
+from fastapi import WebSocket, WebSocketDisconnect
 from schema import Schema, Optional, SchemaError, Regex
-import yaml
-import os
-import uuid
-from yaml import SafeLoader
 from jinja2 import Environment, PackageLoader, select_autoescape
+from yaml import SafeLoader
+from datetime import datetime
+import uuid
+import os
+import yaml
+import asyncio
 
 ##############################################################################
 # Sanity checks
@@ -60,8 +63,32 @@ if not config_schema.is_valid(config):
     raise SchemaError("Invalid schema: hostconf.yaml")
 
 ##############################################################################
+# Defs
+##############################################################################
+
+
+class WebSocketConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+##############################################################################
 # Begin application
 ##############################################################################
+
 
 def get_host_config(client):
     for hostname, host in config.get('hosts').items():
@@ -72,14 +99,46 @@ def get_host_config(client):
 
 
 app = FastAPI(title=__name__)
+wsman = WebSocketConnectionManager()
 env = Environment(
     loader=PackageLoader(__name__),
     autoescape=select_autoescape()
 )
 
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    response = await call_next(request)
+    asyncio.create_task(wsman.broadcast(
+        "%s - %s:%s %s %s %s %s" % (datetime.now(),
+                                    request.client.host, request.client.port,
+                                    request.method, request.url.path,
+                                    "HTTP/1.1", response.status_code)
+    ))
+    return response
+
+
+@app.websocket("/log")
+async def websocket_endpoint(websocket: WebSocket):
+    await wsman.connect(websocket)
+    try:
+        while True:
+            msg = await websocket.receive_text()
+    except WebSocketDisconnect:
+        wsman.disconnect(websocket)
+
+
+@app.get("/log")
+async def log():
+    template = env.get_template("log.j2")
+    return Response(content=template.render({
+        'title': 'hostconf - logs',
+        'host': '192.168.178.20'
+    }), media_type="text/html")
+
+
 @app.get("/osconfig/{client}")
-async def osconfig(client):
+async def osconfig(client, request: Request):
     hostname, host = get_host_config(client)
     template = env.get_template("osconfig.j2")
 
@@ -91,7 +150,7 @@ async def osconfig(client):
 
 
 @app.get("/network-config/{client}")
-async def network_config(client):
+async def network_config(client, request: Request):
     hostname, host = get_host_config(client)
     template = env.get_template("network-config.j2")
 
@@ -101,7 +160,7 @@ async def network_config(client):
 
 
 @app.get("/user-data/{client}")
-async def user_data(client):
+async def user_data(client, request: Request):
     hostname, host = get_host_config(client)
     template = env.get_template("user-data.j2")
 
@@ -115,7 +174,7 @@ async def user_data(client):
 
 
 @app.get("/meta-data/{client}")
-async def meta_data(client):
+async def meta_data(client, request: Request):
     hostname, host = get_host_config(client)
     template = env.get_template("meta-data.j2")
 
@@ -126,7 +185,7 @@ async def meta_data(client):
 
 
 @app.get("/unattend/{client}")
-async def unattend(client):
+async def unattend(client, request: Request):
     hostname, host = get_host_config(client)
     template = env.get_template("unattend.xml.j2")
 
